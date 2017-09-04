22d8f3afadb69f2354a08be25b9ac4346c51cf85
zhangxiandong <13510164430@msn.cn>
2017-09-04 21:58:56 +0800

    提交

--- MTAuto/MainForm.Designer.cs
+++ MTAuto/MainForm.Designer.cs
@@ -35,6 +35,8 @@
             this.MenuItemMain = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemSettings = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.Menu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,41 +56,62 @@
             this.MenuItemSettings,
             this.MenuItemQuit});
             this.Menu.Name = "Menu";
-            this.Menu.Size = new System.Drawing.Size(245, 156);
+            this.Menu.Size = new System.Drawing.Size(161, 112);
             // 
             // MenuItemMain
             // 
             this.MenuItemMain.Name = "MenuItemMain";
-            this.MenuItemMain.Size = new System.Drawing.Size(244, 36);
+            this.MenuItemMain.Size = new System.Drawing.Size(160, 36);
             this.MenuItemMain.Text = "主界面";
             // 
             // MenuItemSettings
             // 
             this.MenuItemSettings.Name = "MenuItemSettings";
-            this.MenuItemSettings.Size = new System.Drawing.Size(244, 36);
+            this.MenuItemSettings.Size = new System.Drawing.Size(160, 36);
             this.MenuItemSettings.Text = "设置";
             // 
             // MenuItemQuit
             // 
             this.MenuItemQuit.Name = "MenuItemQuit";
-            this.MenuItemQuit.Size = new System.Drawing.Size(244, 36);
+            this.MenuItemQuit.Size = new System.Drawing.Size(160, 36);
             this.MenuItemQuit.Text = "退出";
             this.MenuItemQuit.Click += new System.EventHandler(this.MenuItemQuit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "电机控制脚本";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 24);
+            this.label2.TabIndex = 2;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(774, 529);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "凌科自动测试";
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Menu.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -99,6 +122,8 @@
         private System.Windows.Forms.ToolStripMenuItem MenuItemMain;
         private System.Windows.Forms.ToolStripMenuItem MenuItemSettings;
         private System.Windows.Forms.ToolStripMenuItem MenuItemQuit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
